553856f4d10ce7baa01635e8f5d9f828877e3ad6
Ubuntu <ubuntu@ip-172-31-24-40.ec2.internal>
2025-11-17 06:27:16 +0000

    3-idiots Scripts

---
+++ 3-idiots.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+
+hero="Rancho"
+villian="virus"
+
+echo "3 idiots la hero $hero"
+
+echo " 3 idiots ka villain hai $villian"
+
+# shell /envirnment me bhi hota hai
+
+echo "curent logged in user is $USER"
+
+read -p " rancho ka pura nam kua tha?" fullname
+
+echo " rancho ka pura naam $fullname tha."
+
+# argunments
+
+
+#./3-idiots.sh rahu farhan rancho
+
+echo "Movie ka nam $0"
+
+echo "first idiot $1"
+
+echo "second idot $2"
+
+echo "third idiot $3"
+
+echo "hence all idiots Are $@"
+
